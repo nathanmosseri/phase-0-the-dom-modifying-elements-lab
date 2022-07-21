062e6fe714aa7e766a6f642f58e096f2b3f6dbb9
Nathan <namo678@me.com>
2022-07-21 13:13:46 -0400

    commit tree 5fa0e4172fafa3bc971f2b9b0d43f8c0fe06e682

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+main.remove()
+
+// const myName= 'nathan'
+const newHeader = document.createElement('h1')
+newHeader.id= 'victory'
+newHeader.textContent = `is the champion`
+document.body.append(newHeader)
+
+
+
